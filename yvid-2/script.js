dd47a263326cb61b704904fad43c370ccc13b207
Tufahel Ahmed <tufahel96@gmail.com>
2024-08-19 16:37:50 +0600

    node js and npm with file system operations

---
+++ yvid-2/script.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+// fs.writeFile("hello.txt", "hey kaise ho", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.appendFile("hello.txt", " mai to accha hu.", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.rename("hello.txt", "hello_bro.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.copyFile("hello_bro.txt", "./copy/chacha.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.unlink("hello_bro.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.rmdir("./copy", {recursive: true}, function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.readFile('./hello.txt', 'utf8' , (err, data) => {
+//     if (err) throw err;
+//     console.log('kukku rukku: ',data);
+// }); 

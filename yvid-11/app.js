2a90e6669742b0091dc7eb7a6834286c29967bfb
Tufahel Ahmed <tufahel96@gmail.com>
2024-09-10 20:29:53 +0600

    crud operations with ejs and server-side rendering

--- yvid-11/app.js
+++ yvid-11/app.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
+const userModel = require("./models/user");
 
-const userModel = require('./usermodel');
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send("hey");
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/create', async (req, res) => {
-    let createduser = await userModel.create({
-        name: "tufahel",
-        email: "tufahel@gmail.com",
-        username: "tufahel"
-    })
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.render("read", {users});
+});
 
-    res.send(createduser);
-})
+app.get("/delete/:id", async (req, res) => {
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read");
+  });
 
-app.get('/update', async (req, res) => {
-    let updateuser = await userModel.findOneAndUpdate({username: "tufa"}, {name: "omor"}, {new: true});
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+  let createUser = await userModel.create({
+    name,
+    email,
+    image,
+  });
 
-    res.send(updateuser)
-})
-
-app.get('/read', async(req,res) => {
-    let users = await userModel.find();
-    res.send(users);
-})
-
-app.get('/delete', async(req,res) => {
-    let users = await userModel.findOneAndDelete({username: "harsh"});
-    res.send(users);
-})
+  res.redirect("/read");
+});
 
 app.listen(3000);
